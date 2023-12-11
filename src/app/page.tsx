b0f333d33d2746e5d15679b1d72c4f9cfc561440
EdwinDij <edwin.d899@gmail.com>
2023-12-11 16:30:09 +0100

    mise en place de la landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,105 @@
-import Image from 'next/image'
+import Image from "next/image";
+import HeroImg from "../../public/assets/undraw_messages_re_qy9x.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <section className="flex min-h-screen max-lg:flex-col justify-center items-center p-24 max-lg:p-6">
+        <div>
+          <h1 className="text-3xl text-left max-lg:text-center mb-3">
+            Le meilleur moyen de prévenir vos clients
+          </h1>
+          <p className="text-xl text-left max-lg:text-center mb-3">
+            Enregistrer un client, changer ça disponibilité <br></br>et le
+            client est prévenu dans les plus bref délai.
+          </p>
+          <div className="flex gap-4 font-semibold text-lg max-md:justify-center max-md:mb-7">
+            <button className="bg-indigo-500 px-4 py-2 rounded-lg shadow-lg shadow--300 text-white">
+              Get started
+            </button>
+            <button className="px-4 py-2 border border-indigo-400 rounded-lg">
+              plus d{"'"}infos
+            </button>
+          </div>
         </div>
-      </div>
+        <div>
+          <Image src={HeroImg} alt="hero image" width={750} height={540} />
+        </div>
+      </section>
+      <section className="p-24 max-lg:p-6">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-3xl text-center max-lg:text-center mb-20">
+            Qu{"'"}est-ce que propose Avproduct ?
+          </h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className="flex gap-6">
+            <div>
+              <h2 className="text-2xl mb-8">Présentation</h2>
+              <p>
+                Avproduct est une application web facilitant la notification de
+                la disponibilité des produits attendus par vos clients.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-2xl mb-8">Fonctionnalites principales</h2>
+              <p>
+                Les clients enregistrés dans votre tableau de bord sont
+                automatiquement informés par SMS dès que le produit souhaité
+                devient disponible.<br></br>
+                Vous avez la flexibilité de modifier la disponibilité d{"'"}un
+                produit selon vos besoins.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-2xl mb-8">Notification automatisee</h2>
+              <p>
+                Les clients, une fois enregistrés, bénéficient d{"'"}une
+                notification automatique par SMS dès que le produit attendu est
+                disponible.
+              </p>
+            </div>
+            <div>
+              <h2 className="text-2xl mb-8">Gestion disponibilité</h2>
+              <p>
+                En plus de la notification, l{"'"}application offre la
+                possibilité de changer la disponibilité des produits en temps
+                réel. De plus, vous pouvez spécifier la durée pendant laquelle
+                le produit sera maintenu en attente pour le client.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="p-24 max-lg:p-6">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-3xl text-center max-lg:text-center mb-20">
+            Tarification
+          </h1>
+          <div className="flex gap-6">
+            <div>
+              <h2 className="text-2xl mb-8">Plan de base</h2>
+              <p className="mb-8">Prix: €20/mois</p>
+              <ul className="flex flex-col gap-4">
+                <li>Notification automatique par SMS</li>
+                <li>Enregistrement des clients</li>
+                <li>Changement de disponibilité des produits</li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="text-2xl mb-8">Plan annuel</h2>
+              <p className="mb-8">Prix: €200/an</p>
+              <ul className="flex flex-col gap-4">
+                <li>2 mois offerts</li>
+                <li>Toutes les fonctionnalités du plan de base</li>
+                <li>Support 24/7</li>
+              </ul>
+              <button className="bg-indigo-500 px-4 py-2 rounded-lg shadow-lg shadow--300 text-white mt-8">
+                Get started
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
-  )
+  );
 }
